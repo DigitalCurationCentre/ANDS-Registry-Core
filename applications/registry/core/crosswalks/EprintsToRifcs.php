2bd8b90103b7db72adab6be42db98ac83008ba8f
Patrick McCann <patrick.mccann@glasgow.ac.uk>
2014-02-10 16:29:58 +0000

    Moved some functions out to helper class.

--- applications/registry/core/crosswalks/EprintsToRifcs.php
+++ applications/registry/core/crosswalks/EprintsToRifcs.php
@@ -2,17 +2,12 @@
 
 class EprintsToRifcs extends Crosswalk {
 
-	const RIFCS_WRAPPER="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n
-		<registryObjects xmlns=\"http://ands.org.au/standards/rif-cs/registryObjects\"
-		xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
-		xsi:schemaLocation=\"http://ands.org.au/standards/rif-cs/registryObjects
-		http://services.ands.org.au/documentation/rifcs/schema/registryObjects.xsd\">
-		</registryObjects>";
 	private $eprints = null;
 	private $rifcs = null;
 	
 	function __construct(){
-		$this->rifcs = simplexml_load_string($this::RIFCS_WRAPPER);
+		require_once(REGISTRY_APP_PATH . "core/crosswalks/_crosswalk_helper.php");
+		$this->rifcs = simplexml_load_string(CrosswalkHelper::RIFCS_WRAPPER);
 	}
 	
 	public function identify(){
@@ -48,8 +43,8 @@ class EprintsToRifcs extends Crosswalk {
 			$key = null;
 			foreach ($eprint->attributes() as $attribute => $value){
 				if ($attribute == "id") {
-					$key = $reg_obj->addChild("key",$this->escapeAmpersands($value));
-					$reg_obj->addChild("originatingSource",$this->escapeAmpersands($value));
+					$key = $reg_obj->addChild("key",CrosswalkHelper::escapeAmpersands($value));
+					$reg_obj->addChild("originatingSource",CrosswalkHelper::escapeAmpersands($value));
 					break;
 				}
 			}
@@ -90,32 +85,32 @@ class EprintsToRifcs extends Crosswalk {
 	}
 	
 	private function process_id_number($input_node,$output_nodes){
-		$output_nodes["collection"]->addChild("identifier", $this->escapeAmpersands($input_node))->addAttribute("type", "local");
-		if ($this->isUrl($input_node)) {
+		$output_nodes["collection"]->addChild("identifier", CrosswalkHelper::escapeAmpersands($input_node))->addAttribute("type", "local");
+		if (CrosswalkHelper::isUrl($input_node)) {
 			$url = $output_nodes["collection"]->addChild("location")->addChild("address")->addChild("electronic");
 			$url->addAttribute("type","url");
-			$url->addChild("value",$this->escapeAmpersands($input_node));
-			$output_nodes["citation_metadata"]->addChild("url", $this->escapeAmpersands($input_node));
+			$url->addChild("value",CrosswalkHelper::escapeAmpersands($input_node));
+			$output_nodes["citation_metadata"]->addChild("url", CrosswalkHelper::escapeAmpersands($input_node));
 		}
-		$output_nodes["citation_metadata"]->addChild("identifier", $this->escapeAmpersands($input_node))->addAttribute("type", "local");
+		$output_nodes["citation_metadata"]->addChild("identifier", CrosswalkHelper::escapeAmpersands($input_node))->addAttribute("type", "local");
 	}
 	
 	private function process_title($input_node,$output_nodes){
 		$name = $output_nodes["collection"]->addChild("name");
 		$name->addAttribute("type","primary");
-		$name->addChild("namePart", $this->escapeAmpersands($input_node));
-		$output_nodes["citation_metadata"]->addChild("title", $this->escapeAmpersands($input_node));
+		$name->addChild("namePart", CrosswalkHelper::escapeAmpersands($input_node));
+		$output_nodes["citation_metadata"]->addChild("title", CrosswalkHelper::escapeAmpersands($input_node));
 	}
 	
 	private function process_alt_title($input_node,$output_nodes){
 		$name = $output_nodes["collection"]->addChild("name");
 		$name->addAttribute("type","alternative");
-		$name->addChild("namePart", $this->escapeAmpersands($input_node));
+		$name->addChild("namePart", CrosswalkHelper::escapeAmpersands($input_node));
 	}
 	
 	private function process_date_embargo($input_node,$output_nodes){
 		$this->addDate($output_nodes["collection"],"dc.available",$input_node);
-		$citation_date = $output_nodes["citation_metadata"]->addChild("date", $this->escapeAmpersands($input_node));
+		$citation_date = $output_nodes["citation_metadata"]->addChild("date", CrosswalkHelper::escapeAmpersands($input_node));
 		$citation_date->setAttribute("type","available");
 	}
 	
@@ -125,19 +120,19 @@ class EprintsToRifcs extends Crosswalk {
 	
 	private function process_revision($input_node,$output_nodes){
 		$this->addDate($output_nodes["collection"],"dc.issued",$input_node);
-		$output_nodes["citation_metadata"]->addChild("version", $this->escapeAmpersands($input_node));
-		$pub_date = $output_nodes["citation_metadata"]->addChild("date", $this->escapeAmpersands($input_node));
+		$output_nodes["citation_metadata"]->addChild("version", CrosswalkHelper::escapeAmpersands($input_node));
+		$pub_date = $output_nodes["citation_metadata"]->addChild("date", CrosswalkHelper::escapeAmpersands($input_node));
 		$pub_date->addAttribute("type","publicationDate,issued");
 	}
 	
 	private function process_keywords($input_node,$output_nodes){
-		$subject = $output_nodes["collection"]->addChild("subject", $this->escapeAmpersands($input_node));
+		$subject = $output_nodes["collection"]->addChild("subject", CrosswalkHelper::escapeAmpersands($input_node));
 		$subject->addAttribute("type","local");
 	}
 	
 	private function process_subjects($input_node,$output_nodes){//items
 		foreach ($input_node->children() as $item) {
-			$subject = $output_nodes["collection"]->addChild("subject", $this->escapeAmpersands($item));
+			$subject = $output_nodes["collection"]->addChild("subject", CrosswalkHelper::escapeAmpersands($item));
 			$subject->addAttribute("type","local");
 		}
 	}
@@ -155,21 +150,21 @@ class EprintsToRifcs extends Crosswalk {
 	}
 	
 	private function process_temporal_cover($input_node,$output_nodes){
-		$output_nodes["coverage"]->addChild("temporal")->addChild("date",$this->escapeAmpersands($input_node));
+		$output_nodes["coverage"]->addChild("temporal")->addChild("date",CrosswalkHelper::escapeAmpersands($input_node));
 	}
 	
 	private function process_geographic_cover($input_node,$output_nodes){	
-		$output_nodes["coverage"]->addChild("spatial",$this->escapeAmpersands($input_node))->addAttribute("type","text");
+		$output_nodes["coverage"]->addChild("spatial",CrosswalkHelper::escapeAmpersands($input_node))->addAttribute("type","text");
 	}
 	
 	private function process_bounding_box($input_node,$output_nodes){
-		$output_nodes["coverage"]->addChild("spatial",$this->escapeAmpersands($input_node))->addAttribute("type","iso19139dcmiBox");
+		$output_nodes["coverage"]->addChild("spatial",CrosswalkHelper::escapeAmpersands($input_node))->addAttribute("type","iso19139dcmiBox");
 	}
 	
 	private function process_related_resources($input_node,$output_nodes){
 		$related_info = $output_nodes["collection"]->addChild("relatedInfo");
 		$related_info->addAttribute("type","collection");
-		$related_info->addChild("identifier",$this->escapeAmpersands($input_node))->addAttribute("type", "local");
+		$related_info->addChild("identifier",CrosswalkHelper::escapeAmpersands($input_node))->addAttribute("type", "local");
 		foreach ($input_node->children() as $child) {
 			if ($child->getName() == "relationType") {
 				$relation = $related_info->addChild("relation");
@@ -179,19 +174,19 @@ class EprintsToRifcs extends Crosswalk {
 	}
 	
 	private function process_security($input_node,$output_nodes){
-		$output_nodes["collection"]->addChild("rights")->addChild("accessRights",$this->escapeAmpersands($input_node));
+		$output_nodes["collection"]->addChild("rights")->addChild("accessRights",CrosswalkHelper::escapeAmpersands($input_node));
 	}
 	
 	private function process_restrictions($input_node,$output_nodes){
-		$output_nodes["collection"]->addChild("rights")->addChild("accessRights",$this->escapeAmpersands($input_node));
+		$output_nodes["collection"]->addChild("rights")->addChild("accessRights",CrosswalkHelper::escapeAmpersands($input_node));
 	}
 	
 	private function process_accessLimitations($input_node,$output_nodes){
-		$output_nodes["collection"]->addChild("rights")->addChild("accessRights",$this->escapeAmpersands($input_node));
+		$output_nodes["collection"]->addChild("rights")->addChild("accessRights",CrosswalkHelper::escapeAmpersands($input_node));
 	}
 	
 	private function process_license($input_node,$output_nodes){
-		$output_nodes["collection"]->addChild("rights")->addChild("licence",$this->escapeAmpersands($input_node));
+		$output_nodes["collection"]->addChild("rights")->addChild("licence",CrosswalkHelper::escapeAmpersands($input_node));
 	}
 	
 	private function process_creators($input_node,$output_nodes){
@@ -202,26 +197,26 @@ class EprintsToRifcs extends Crosswalk {
 				if ($child->getName() == "name") {
 					foreach ($child->children() as $name_part) {
 						if ($name_part->getName() == "given") {
-							$name = $this->escapeAmpersands($name_part)." ".$name;
+							$name = CrosswalkHelper::escapeAmpersands($name_part)." ".$name;
 						}
 						elseif ($name_part->getName() == "family") {
-							$name = $name.$this->escapeAmpersands($name_part);
+							$name = $name.CrosswalkHelper::escapeAmpersands($name_part);
 						}
 					}
-					$output_nodes["citation_metadata"]->addChild("contributor")->addChild("namePart", $this->escapeAmpersands($name));
+					$output_nodes["citation_metadata"]->addChild("contributor")->addChild("namePart", CrosswalkHelper::escapeAmpersands($name));
 				}
 				elseif ($child->getName() == "id") {
-					$id = $this->escapeAmpersands($child);
+					$id = CrosswalkHelper::escapeAmpersands($child);
 				}
 			}
 			if ($id != null) {
 				$reg_obj = $this->rifcs->addChild("registryObject");
 				$reg_obj->addAttribute("group","EPrints");
 				$reg_obj->key = $id;
-				$reg_obj->originatingSource = $this->escapeAmpersands($output_nodes["key"]);
+				$reg_obj->originatingSource = CrosswalkHelper::escapeAmpersands($output_nodes["key"]);
 				$party = $reg_obj->addChild("party");
 				$party->addAttribute("type","person");
-				$party->addChild("name")->namePart = $this->escapeAmpersands($name);
+				$party->addChild("name")->namePart = CrosswalkHelper::escapeAmpersands($name);
 				$rel_obj = $party->addChild("relatedObject");
 				$rel_obj->key = $output_nodes["key"];
 				$rel_obj->addChild("relation")->addAttribute("type","isCollectorOf");
@@ -230,37 +225,19 @@ class EprintsToRifcs extends Crosswalk {
 	}
 	
 	private function process_publisher($input_node,$output_nodes){
-		$output_nodes["citation_metadata"]->addChild("publisher",$this->escapeAmpersands($input_node));
+		$output_nodes["citation_metadata"]->addChild("publisher",CrosswalkHelper::escapeAmpersands($input_node));
 	}
 	
 	private function addDate($collection,$type,$value){
 		$dates = $collection->addChild("dates");
 		$dates->addAttribute("type", $type);
-		$dates_date = $dates->addChild("date",$this->escapeAmpersands($value));
+		$dates_date = $dates->addChild("date",CrosswalkHelper::escapeAmpersands($value));
 		$dates_date->addAttribute("type", "dateFrom");
 		$dates_date->addAttribute("dateFormat", "W3CDTF");
 	}
 	
 	private function addDescription($collection,$type,$value){
-		$description = $collection->addChild("description", $this->escapeAmpersands($value));
+		$description = $collection->addChild("description", CrosswalkHelper::escapeAmpersands($value));
 		$description->addAttribute("type",$type);
-	}
-	
-	private function escapeAmpersands($string){
-		return str_replace("&", "&amp;", $string);
-	}
-	
-	private function isUrl($string){
-		//regex from https://gist.github.com/gruber/8891611
-		$match = preg_match(
-			"#(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))#",
-			$string
-		);
-		if ($match == 1) {
-			return true;
-		}
-		else {
-			return false;
-		}		
 	}
 }
