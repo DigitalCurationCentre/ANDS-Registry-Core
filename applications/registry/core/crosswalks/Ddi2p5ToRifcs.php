072400a4697850f4272a643a4ee457e256d9e00e
Alex Ball <a.ball@ukoln.ac.uk>
2014-02-12 18:48:21 +0000

    Set up new (incomplete) crosswalk for DDI 2.5 to RIF-CS.

---
+++ applications/registry/core/crosswalks/Ddi2p5ToRifcs.php
@@ -0,0 +1,192 @@
+<?php
+
+class Ddi2p5ToRifcs extends Crosswalk {
+
+	private $oaipmh = null;
+	private $rifcs = null;
+	private $ddiProviders = array(
+		"http://oai.ukdataservice.ac.uk/oai/provider" => "UK Data Archive",
+	);
+	
+	function __construct(){
+		require_once(REGISTRY_APP_PATH . "core/crosswalks/_crosswalk_helper.php");
+		$this->rifcs = simplexml_load_string(CrosswalkHelper::RIFCS_WRAPPER);
+	}
+	
+	public function identify(){
+		return "DDI v2.5 to RIF-CS (Experimental)";
+	}
+	
+	public function metadataFormat(){
+		return "ddi_2.5";
+	}
+	
+	public function validate($payload){
+		$this->load_payload($payload);
+		if (!$this->oaipmh){
+			return false;
+		}
+		if ($this->oaipmh->getName() != "OAI-PMH") {
+			return false;
+		}
+		if (empty($this->oaipmh->request)) {
+			return false;
+		}
+		if (empty($this->oaipmh->ListRecords)) {
+			return false;
+		}
+		foreach($this->oaipmh->ListRecords as $record) {
+			if ($record->getName() == "record") {
+				if (empty($record->header->identifier)) {
+					return false;
+				}
+				if (empty($record->metadata->codeBook->stdyDscr)) {
+					return false;
+				}
+			}
+		}
+		//More validation needed...
+		return true;
+	}
+	
+	public function payloadToRIFCS($payload){
+		$this->load_payload($payload);
+		foreach ($this->oaipmh->ListRecords as $record){
+			if ($record->getName() != "record") {
+				continue;
+			}
+			$reg_obj = $this->rifcs->addChild("registryObject");
+			if (array_key_exists($this->oaipmh->request, $this->ddiProviders) {
+				$reg_obj->addAttribute("group", $this->ddiProviders[$this->oaipmh->request]);
+			}
+			$key = $reg_obj->addChild("key", $record->header->identifier);
+			$originatingSource = $reg_obj->addChild("originatingSource", $this->oaipmh->request);
+			$coll = $reg_obj->addChild("collection");
+			$coll->addAttribute("type", "dataset");
+			$citation = $coll->addChild("citationInfo");
+			$citation_metadata = $citation->addChild("citationMetadata");
+			$coverage = $coll->addChild("coverage");
+			foreach ($record->metadata->codeBook->stdyDscr->children() as $node){
+				foreach ($node->children as $subnode) {
+					$func = "process_".$subnode->getName();
+					if (is_callable(array($this, $func))){
+						call_user_func(
+							array($this, $func),
+							$subnode,
+							array(
+								"registry_object" => $reg_obj,
+								"key" => $key,
+								"collection" => $coll,
+								"citation_metadata" => $citation_metadata,
+								"coverage" => $coverage
+							)
+						);
+					}
+				}
+			}
+		}
+		return $this->rifcs->asXML();
+	}
+	
+	private function load_payload($payload){
+		if ($this->oaipmh == null) {
+			$this->oaipmh = simplexml_load_string($payload);
+		}
+	}
+	
+	private function addDate($collection,$type,$value){
+		$dates = $collection->addChild("dates");
+		$dates->addAttribute("type", $type);
+		$dates_date = $dates->addChild("date",CrosswalkHelper::escapeAmpersands($value));
+		$dates_date->addAttribute("type", "dateFrom");
+		$dates_date->addAttribute("dateFormat", "W3CDTF");
+	}
+	
+	private function addDescription($collection,$type,$value){
+		$description = $collection->addChild("description", CrosswalkHelper::escapeAmpersands($value));
+		$description->addAttribute("type",$type);
+	}
+	
+	private function process_titlStmt($input_node,$output_nodes){
+		foreach ($input_node->children() as $stmt) {
+			switch ($stmt->getName()) {
+			case "titl":
+				$name = $output_nodes["collection"]->addChild("name", $stmt);
+				$name->addAttribute("type", "primary");
+				$output_nodes["citation_metadata"]->addChild("title", $stmt);
+				break;
+			case "altTitl":
+				$altName = $output_nodes["collection"]->addChild("name", $stmt);
+				$altName->addAttribute("type", "alternative");
+				break;
+			case "IDNo":
+				if ($stmt["agency"] == "datacite") {
+					$idNo = $output_nodes["collection"]->addChild("identifier", $stmt);
+					$idNo->addAttribute("type", "doi");
+					$citIdNo = $output_nodes["citation_metadata"]->addChild("identifier", $stmt);
+					$citIdNo->addAttribute("type", "doi");
+				} elseif ($stmt["agency"] == "UKDA") {
+					$idNo = $output_nodes["collection"]->addChild("identifier", "sn" . $stmt);
+					$idNo->addAttribute("type", "local");
+					$relInfo = $output_nodes["collection"]->addChild("relatedInfo");
+					$relInfo->addAttribute("type", "metadata");
+					$relIdNo = $relInfo->addChild("identifier", "http://esds.ac.uk/DDI25/" . $stmt . ".xml");
+					$relIdNo->addAttribute("type", "uri");
+					$relInfoFmt = $relInfo->addChild("format");
+					$relInfoFmtIdNo = $relInfoFmt->addChild("identifier", "http://www.ddialliance.org/Specification/DDI-Codebook/2.5/XMLSchema/codebook.xsd");
+					$relInfoFmtIdNo->addAttribute("type", "uri");
+				} else {
+					$idNo = $output_nodes["collection"]->addChild("identifier", $stmt);
+					$idNo->addAttribute("type", "local");
+				}
+				break;
+			}
+		}
+		$output_nodes["collection"]->addAttribute("dateAccessioned", $input_node->distStmt->depDate["date"]);
+	}
+	
+	private function process_rspStmt($input_node,$output_nodes){
+	}
+	
+	private function process_prodStmt($input_node,$output_nodes){
+	}
+	
+	private function process_distStmt($input_node,$output_nodes){
+	}
+	
+	private function process_verStmt($input_node,$output_nodes){
+	}
+	
+	private function process_holdings($input_node,$output_nodes){
+	}
+	
+	private function process_subject($input_node,$output_nodes){
+	}
+	
+	private function process_abstract($input_node,$output_nodes){
+	}
+	
+	private function process_sumDscr($input_node,$output_nodes){
+	}
+	
+	private function process_dataColl($input_node,$output_nodes){
+	}
+	
+	private function process_setAvail($input_node,$output_nodes){
+	}
+	
+	private function process_useStmt($input_node,$output_nodes){
+	}
+	
+	private function process_relStdy($input_node,$output_nodes){
+	}
+	
+	private function process_relPubl($input_node,$output_nodes){
+	}
+	
+	private function process_othRefs($input_node,$output_nodes){
+	}
+
+}
+
+?>
