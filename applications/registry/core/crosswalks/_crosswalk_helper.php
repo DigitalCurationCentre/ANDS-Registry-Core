0c6fe4937415bf01f5d89d0e9d43c6a3d8857966
Alex Ball <a.ball@ukoln.ac.uk>
2014-03-10 18:45:35 +0000

    Added function for recognising URIs.

--- applications/registry/core/crosswalks/_crosswalk_helper.php
+++ applications/registry/core/crosswalks/_crosswalk_helper.php
@@ -30,5 +30,12 @@ class CrosswalkHelper {
 			return false;
 		}		
 	}
+	
+	public static function isUri($string) {
+		if (preg_match('~^(?:aaa|aaas|about|acap|acct|adiumxtra|afp|afs|aim|apt|attachment|aw|beshare|bitcoin|bolo|callto|cap|chrome|chrome-extension|cid|coap|coaps|com-eventbrite-attendee|content|crid|cvs|data|dav|dict|dlna-playcontainer|dlna-playsingle|dns|dtn|dvb|ed2k|facetime|fax|feed|feedready|file|finger|fish|ftp|geo|gg|git|gizmoproject|go|gopher|gtalk|h323|ham|hcp|http|https|iax|icap|icon|im|imap|info|ipn|ipp|irc|irc6|ircs|iris|iris.beep|iris.lwz|iris.xpc|iris.xpcs|itms|jabber|jar|jms|keyparc|lastfm|ldap|ldaps|magnet|mailserver|mailto|maps|market|message|mid|mms|modem|ms-help|ms-settings-power|msnim|msrp|msrps|mtqp|mumble|mupdate|mvn|news|nfs|ni|nih|nntp|notes|oid|opaquelocktoken|pack|palm|paparazzi|pkcs11|platform|pop|pres|prospero|proxy|psyc|query|reload|res|resource|rmi|rsync|rtmp|rtsp|rtsps|rtspu|secondlife|service|session|sftp|sgn|shttp|sieve|sip|sips|skype|smb|sms|snews|snmp|soap.beep|soap.beeps|soldat|spotify|ssh|steam|stun|stuns|svn|tag|teamspeak|tel|telnet|tftp|things|thismessage|tip|tn3270|turn|turns|tv|udp|unreal|urn|ut2004|vemmi|ventrilo|videotex|view-source|wais|webcal|ws|wss|wtai|wyciwyg|xcon|xcon-userid|xfire|xmlrpc.beep|xmlrpc.beeps|xmpp|xri|ymsgr|z39.50|z39.50r|z39.50s):\S+~', $string)) {
+			return true;
+		}
+		return false;
+	}
 
 }
