4c7697648fd7a5f83b2fdc24fbb77761f8e007e8
Alex Ball <a.ball@ukoln.ac.uk>
2014-03-18 14:17:24 +0000

    Set up stub crosswalk from DataCite to RIF-CS.

---
+++ applications/registry/core/crosswalks/DataCite3ToRifcs.php
@@ -0,0 +1,188 @@
+<?php
+
+class DataCite3ToRifcs extends Crosswalk {
+
+	private $oaipmh = null;
+	private $rifcs = null;
+	
+	function __construct(){
+		require_once(REGISTRY_APP_PATH . "core/crosswalks/_crosswalk_helper.php");
+		$this->rifcs = simplexml_load_string(CrosswalkHelper::RIFCS_WRAPPER);
+	}
+	
+	public function identify(){
+		return "DataCite (up to version 3) to RIF-CS (Experimental)";
+	}
+	
+	public function metadataFormat(){
+		return "oai_datacite";
+	}
+	
+	public function validate($payload){
+		$this->load_payload($payload);
+		if (!$this->oaipmh){
+			return false;
+		}
+		if ($this->oaipmh->getName() != "OAI-PMH") {
+			return false;
+		}
+		if (empty($this->oaipmh->request)) {
+			return false;
+		}
+		if (empty($this->oaipmh->ListRecords)) {
+			return false;
+		}
+		return true;
+	}
+	
+	public function payloadToRIFCS($payload){
+		$this->load_payload($payload);
+		foreach ($this->oaipmh->ListRecords->children() as $record){
+			if ($record->getName() != "record") {
+				continue;
+			}
+			$reg_obj = $this->rifcs->addChild("registryObject");
+			if (isset(CrosswalkHelper::$oaipmhProviders[(string) $this->oaipmh->request])) {
+				$reg_obj->addAttribute("group", CrosswalkHelper::$oaipmhProviders[(string) $this->oaipmh->request]);
+			} else {
+				$reg_obj->addAttribute("group", $this->oaipmh->request);
+			}
+			$key = $reg_obj->addChild("key", $record->header->identifier);
+			$originatingSource = $reg_obj->addChild("originatingSource", $this->oaipmh->request);
+			$coll = $reg_obj->addChild("collection");
+			$coll->addAttribute("type", "dataset");
+			$coll->addAttribute("dateModified", date(DATE_W3C));
+			$citation = $coll->addChild("citationInfo");
+			$citation_metadata = $citation->addChild("citationMetadata");
+			$coverage = $coll->addChild("coverage");
+			$rights = $coll->addChild("rights");
+			$contributors = array();
+			foreach ($record->metadata->oai_datacite->payload->resource->children() as $node) {
+				$func = "process_".$node->getName();
+				if (is_callable(array($this, $func))){
+					call_user_func(
+						array($this, $func),
+						$node,
+						array(
+							"registry_object" => $reg_obj,
+							"key" => $key,
+							"collection" => $coll,
+							"citation_metadata" => $citation_metadata,
+							"coverage" => $coverage,
+							"rights" => $rights,
+							"contributors" => &$contributors
+						)
+					);
+				}
+			}
+		}
+		return $this->rifcs->asXML();
+	}
+	
+	private function load_payload($payload){
+		if ($this->oaipmh == null) {
+			$this->oaipmh = simplexml_load_string($payload);
+		}
+	}
+	
+	private function translateIdentifierType($string){
+		$idType = "local";
+		$idTypes = array(
+			"DOI" => "doi",
+		);
+		if (isset($idTypes[$string])) {
+			$idType = $idTypes[$string];
+		}
+		return $idType;
+	}
+	
+	private function process_identifier($input_node, $output_nodes) {
+		$id = $output_nodes["collection"]->addChild("identifier", (string) $input_node);
+		$idType = $this->translateIdentifierType((string) $input_node["identifierType"]);
+		$id->addAttribute("type", $idType);
+		
+		$cite_id = $output_nodes["citation_metadata"]->addChild("identifier", (string) $input_node);
+		$cite_id->addAttribute("type", $idType);
+		
+		if ($idType == "doi") {
+			$location = $output_nodes["collection"]->addChild("location");
+			$address = $location->addChild("address");
+			$electronic = $address->addChild("electronic");
+			$electronic->addAttribute("url");
+			$electronic->addChild("value", "http://dx.doi.org/" . (string) $input_node);
+			
+			$output_nodes["citation_metadata"]->addChild("url", "http://dx.doi.org/" . (string) $input_node);
+		}
+	}
+	
+	private function process_creators($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_titles($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_publisher($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_publicationYear($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_subjects($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_contributors($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_dates($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_language($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_resourceType($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_alternateIdentifiers($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_relatedIdentifiers($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_sizes($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_formats($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_version($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_rightsList($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_descriptions($input_node, $output_nodes) {
+		
+	}
+	
+	private function process_geoLocations($input_node, $output_nodes) {
+		
+	}
+
+}
+
+?>
